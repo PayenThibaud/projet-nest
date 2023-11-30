889d4a366b77981735af774821d3d00cbeb46e7b
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-11-30 15:16:45 +0100

    docs: array work

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,7 +6,7 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get("Product")
-  nouvelle(): string {
+  nouvelle(): string[] {
     return this.productService.nouvelleRoute();
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductService {
-  nouvelleRoute(): string {
-    return 'Hello product';
+  nouvelleRoute(): string[] {
+    let table: string[] = ['article 1', "article 2", "article 3"];
+    return table;
 }
 }
