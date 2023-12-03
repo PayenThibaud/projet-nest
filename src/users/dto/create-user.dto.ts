2499621ade728e257f60facb3cbed98d803b9cff
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-03 22:46:12 +0100

    docs: swagger decorateur

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,22 @@
 import { IsEmail, IsString, Length } from "class-validator"
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+    @ApiProperty({
+        description: 'This field represents the new user nickname',
+        minLength: 5,
+        maxLength: 50,
+      })
     @IsString()
     @Length(5, 50)
     pseudo: string;
 
+    @ApiProperty({
+        description: 'This field represents the new user mail address',
+        minLength: 2,
+        maxLength: 75,
+        example: 'mymail@gmail.com',
+      })
     @IsEmail()
     @Length(2, 75)
     mail: string;
