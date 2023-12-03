39dc66f1c3117a92bd165a2ffffa567d44364af4
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-03 23:00:27 +0100

    docs: route logique panier

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import NormalizedResponse from 'src/utils/normalized-response';
 
 @Injectable()
 export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+
   nouvelleRoute(): string[] {
    return  ['article 1', "article 2", "article 3"];
+}
+public async create(createProductDto: CreateProductDto) {
+  return new NormalizedResponse(
+    `Product ${createProductDto.name} has been registered`,
+    await this.prisma.products.create({
+      data: {
+        Name: createProductDto.name,
+        Description: createProductDto.description,
+        Price: createProductDto.price,
+        Author: {
+          connect: {
+            UUID: createProductDto.owner_uuid,
+          },
+        },
+      },
+    }),
+  ).toJSON();
 }
 }
